e2ef5d791481ba125cbc23cd6fe18481410ab917
Egor <05murik25@mail.ru>
2022-10-24 20:35:56 +0300

    add models & register models

--- certificate/app/admin.py
+++ certificate/app/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 from .models import *
+
+admin.site.register(Attribute)
+admin.site.register(Component)
+admin.site.register(Html)
+admin.site.register(Layout)
--- certificate/app/models.py
+++ certificate/app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Attribute(models.Model):
+    color = models.CharField(max_length=16)
+    font = models.CharField(max_length=24)
+    font_size = models.CharField(max_length=8)
+    font_weight = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f'{self.color} :: {self.font}'
+
+
+class Component(models.Model):
+    type = models.CharField(max_length=24)
+    x = models.CharField(max_length=8)
+    y = models.CharField(max_length=8)
+    z = models.CharField(max_length=8)
+    width = models.CharField(max_length=8)
+    height = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f'{self.type}'
+
+
+class Html(models.Model):
+    name = models.CharField(max_length=16, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Layout(models.Model):
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    html = models.ForeignKey(Html, on_delete=models.CASCADE)
