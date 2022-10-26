9fea821bdc257cd47d3a792fb82cecfca0d02a70
adi <sergeq198.97@yandex.ru>
2022-10-26 18:25:49 +0800

    c

--- certificate/certificate/settings.py
+++ certificate/certificate/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if not os.getenv('PROD', False):
+    from dotenv import load_dotenv
+    load_dotenv(os.path.join(BASE_DIR, 'setting.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET')
+SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
