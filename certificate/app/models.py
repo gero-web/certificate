12e114d953c2de7921ce9a66106876ca07de8624
Egor <05murik25@mail.ru>
2022-11-04 17:59:46 +0300

    fix

--- certificate/app/models.py
+++ certificate/app/models.py
@@ -17,12 +17,12 @@ def upload_to(instance, filename):
 
 class Component(models.Model):
     type = models.ForeignKey(TypeComponent, on_delete=models.CASCADE)
-    color = models.CharField(max_length=16,  blank=True, null=True)
+    color = models.CharField(max_length=30,  blank=True, null=True)
     font = models.CharField(max_length=24, blank=True, null=True)
     font_size = models.CharField(max_length=8,  blank=True, null=True)
     font_weight = models.CharField(max_length=8, blank=True, null=True)
-    x = models.CharField(max_length=8)
-    y = models.CharField(max_length=8)
+    x = models.CharField(max_length=8, null=True, blank=True)
+    y = models.CharField(max_length=8, null=True, blank=True)
     width = models.CharField(max_length=8, blank=True, null=True)
     height = models.CharField(max_length=8, blank=True, null=True)
     text = models.TextField(blank=True, null=True)
