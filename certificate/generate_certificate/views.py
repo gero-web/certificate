9d2a7dc53d45adc3318099f51a4fc35622b9554d
Local Admin <sergeq192.gmail.com>
2022-11-08 09:31:26 +0800

    excel

--- certificate/generate_certificate/views.py
+++ certificate/generate_certificate/views.py
@@ -1,20 +1,24 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import TemplateHTMLRenderer,JSONRenderer
 from rest_framework.decorators import api_view, renderer_classes, \
     parser_classes
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
+from django.http.response import JsonResponse
 from app.models import Component
 from app.serializers.invalidSerializers import InvalidSerializer
 from generate_certificate.serializers.exelserializer import ExcelSerializers
 import pandas as pd
-
+from django.core.files.uploadedfile import InMemoryUploadedFile;
 
 @api_view(['GET'])
-@renderer_classes([TemplateHTMLRenderer])
+@renderer_classes([JSONRenderer])
 def Certificate(request, layout_key):
+     
     components = Component.objects.filter(layout__layout_key=layout_key)
+    if( not components):
+          return JsonResponse(data={'msg': 'layout not found'}, status=status.HTTP_404_NOT_FOUND)
     return Response({'components': components}, template_name='crificate.html')
 
 
@@ -28,9 +32,15 @@ def Certificate(request, layout_key):
 def postExcel(request):
     excelSerializer = ExcelSerializers(data=request.data)
     if excelSerializer.is_valid():
-        excel = request.data['excel']
-        df = pd.read_excel(excel, sheet_name='Sheet1')
-        print(df)
+        #excel only
+        excel:InMemoryUploadedFile = request.data['excel']
+        typeFile = excel.name[excel.name.rindex('.') + 1:] 
+        df = pd.read_excel(excel)
+        lst = list(zip(*([*i.values()] for i in df.to_dict().values())))
+        print(df.to_dict())
+        print(lst)
     components = Component.objects.filter(layout__layout_key=excelSerializer.data['layout_key'])
+    if( not components):
+          return JsonResponse(data={'msg': 'layout not found'}, status=status.HTTP_404_NOT_FOUND)
     return Response({'components': components}, status=status.HTTP_200_OK,
                     template_name='crificate.html')
