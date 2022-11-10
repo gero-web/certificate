696b07cbfbc7e3904711785450025d8be6d44561
adi <sergeq198.97@yandex.ru>
2022-11-11 00:23:00 +0800

    fix: cross

--- certificate/certificate/settings.py
+++ certificate/certificate/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOW_CREDENTIALS = True
 
 #CORS_ALLOWED_ORIGINS = [
  #   'http://localhost:3030',
