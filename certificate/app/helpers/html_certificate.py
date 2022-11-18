4a0fb0d55f831e45be27b62d6c5ace8d22d4c092
adi <sergeq198.97@yandex.ru>
2022-11-18 23:28:31 +0800

    view email per message

--- certificate/app/helpers/html_certificate.py
+++ certificate/app/helpers/html_certificate.py
@@ -1,12 +1,21 @@
+from django.conf import settings
 from django.template.loader import get_template
 from app.models import Component
+
 
 
 def html_template_certificate(certificate_key):
     components = Component.objects.filter(certificate__certificate_key=certificate_key)
     if not components:
         return None
-    context = { 'components': components,}
-
+    context = {'components': components, }
     html_content = get_template('crificate.html').render(context)
+    return html_content
+
+
+def html_url_certificate(certificate_key):
+
+    context = {'certificate_key': certificate_key,
+               'domain': settings.SITE_URL, }
+    html_content = get_template('url.html').render(context)
     return html_content
--- certificate/app/urls.py
+++ certificate/app/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from .views.ComponentViews import ComponentViewsSet
 from .views.typeComponentViews import TypeComponentViewsSet
 from .views.certificateViews import CertificateViewsSet
 from .views.layoutViews import LayoutViewsSet
 from .views.pdf_view import getPdf
+from .views.email_view import email
 
 router = DefaultRouter()
 router.register('component', ComponentViewsSet, basename='component')
@@ -12,8 +13,10 @@ router.register('type_component', TypeComponentViewsSet, basename='type_componen
 router.register('layout', LayoutViewsSet, basename='layout')
 router.register('certificate', CertificateViewsSet, basename='certificate')
 
-urlpatterns = [
-                  path('', include(router.urls)),
-                  path('pdf/<slug:certificate_key>/', getPdf, name='get_pdf'),
 
-              ]
+urlpatterns = [
+    path('', include((router.urls, 'app_name'), namespace='api')),
+    path('pdf/<slug:certificate_key>/', getPdf, name='get_pdf'),
+    path('email/<slug:certificate_key>/', email,
+            name='email')
+]
