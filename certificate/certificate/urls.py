8c0fcefe4e6860816f5fee6fa6502c2c329e0586
gero <sergeq198.97@yandex.ru>
2022-11-27 00:21:07 +0800

    check

--- certificate/certificate/urls.py
+++ certificate/certificate/urls.py
@@ -22,7 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('api/', include('app.urls')),
    
     # swagger
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
