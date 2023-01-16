e73997ed21dacb8081519b75eeb115cadac039a8
gero-web <sergeq198@yandex.ru>
2023-01-16 19:04:03 +0800

    size pdf a4 and a3

--- certificate/app/views/pdf_creator.py
+++ certificate/app/views/pdf_creator.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from app.serializers.invalidSerializers import InvalidSerializer
 from drf_spectacular.utils import extend_schema
 from app.helpers.html_certificate import html_template_certificate
-from img2pdf import convert
+from img2pdf import convert, mm_to_pt, get_layout_fun
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from app.serializers.pdfGeneratorImageSerializers import PdfGeneratorImageSerializers
@@ -30,10 +30,13 @@ def render_to_pdf(req):
         for data in  req_file.data['image']:
             file = base64.b64decode(data.split('base64,')[1])
             file = io.BytesIO(file)  
-        pdf = convert(file)
+        letter = (mm_to_pt(210), mm_to_pt(297)) # A4
+        # letter = (mm_to_pt(420), mm_to_pt(297)) # A3
+        layout = get_layout_fun(letter)
+        pdf = convert(file, layout_fun = layout)
        
         response = HttpResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename=certificate'  
+       # response['Content-Disposition'] = 'attachment; filename=certificate'  
         return response
     else:
          return HttpResponse('body empty', status= status.HTTP_400_BAD_REQUEST)
